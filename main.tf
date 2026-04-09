943e30ca5847f586f95e920ff6f3471e37362b70
Sadhix <moizhussain89569@gmail.com>
2026-04-09 21:49:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "nova-tf-state907081"
+    bucket = "admin-bucksadix"
     key    = "ec2/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-2"
   }
 }
 
